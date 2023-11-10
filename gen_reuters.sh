c4a2c373c34a9a818406fb5f5208c3754fc616a2
itsaquestion <itsaquestion@gmail.com>
2023-11-10 13:38:50 +0800

    quarto init

---
+++ gen_reuters.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Navigate to the script's directory
+cd "$(dirname "$0")"
+
+# Activate the virtual environment
+source ./venv/bin/activate
+
+# Run the Python script
+python ./gen_reuters.py
+
